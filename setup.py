ad8f043c6f4aa1df978979a22ceb8884f3bc73cb
Sam Johnston <samj@samj.net>
2024-06-06 01:29:44 -0700

    expand setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,34 @@
-#!/usr/bin/env python3
 from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="fetch_llama_cpp",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
+    install_requires=[
+        "requests",
+        "py-cpuinfo",
+        "packaging",
+    ],
     entry_points={
         'console_scripts': [
             'fetch_llama_cpp = fetch_llama_cpp:fetch',
         ],
     },
+    author="Sam Johnston",
+    author_email="samj@samj.net",
+    description="A package to fetch the latest and best version of llama.cpp for your system programmatically.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/yourusername/fetch_llama_cpp",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
